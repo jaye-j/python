234aaefe7df65e1755d84e66b427067d5209b625
Jaye Jensen <jacobrobertjensen@gmail.com>
2020-01-14 16:03:17 -0600

    homework day 2 jan 14 2020

---
+++ hwday2.py
@@ -0,0 +1,85 @@
+#Homework day 2 Tuesday January 14th 2020
+
+#Small
+
+#1 Hello You!
+
+# name = input("What is your name?")
+# namelen = len(name)
+# if namelen > 0 :
+#     print("Hello, " + name)
+
+
+#2 HELLO YOU!
+
+# name = input("What is your name?")
+# namelen = len(name)
+# if namelen > 0 :
+#     print("HELLO " + name.upper())
+#     print("YOUR NAME HAS " + str(namelen) + " LETTERS IN IT! AWESOME!")
+
+
+#3 Madlib
+
+# madlib = "Please fill the blanks below: \n____(name)____'s favorite subject in school is ____(subject)____."
+# print(madlib)
+
+# name = input("What is name?")
+# subject = input("What is Subject?")
+# namelen = len(name)
+# sublen = len(subject)
+
+# if namelen > 0 and sublen > 0 :
+#     madlibanswered = f"{name}'s favorite subject is {subject}."
+#     print(madlibanswered)
+
+
+#4 Day of the Week
+
+# day = int(input('Day (0-6)? '))
+
+# if day == 0 :
+#     print("Sunday")
+
+# elif day == 1 :
+#     print("Monday")
+
+# elif day == 2 :
+#     print("Tuesday")
+
+# elif day == 3 :
+#     print("Wednesday")
+
+# elif day == 4 :
+#     print("Thursday")
+
+# elif day == 5 :
+#     print("Friday")
+
+# elif day == 6 :
+#     print("Saturday")
+
+# else:
+#     print("Invalid number!")
+
+
+#5 Work or Sleep In?
+
+# day = int(input('Day (0-6)? '))
+
+# if day == 0 :
+#     print("Sleep in!")
+
+# elif day > 0 and day < 6 :
+#     print("Go to work.")
+
+# elif day == 6 :
+#     print("Sleep in!")
+
+# else:
+#     print("Invalid number!")
+
+
+#6 Celsius to Fahrenheit
+
+C = int(input("Whats the temperature in Celsius? "))
