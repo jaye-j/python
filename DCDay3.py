027a429bf733a70cf221e9b6dfa2a075ca313f46
Jaye Jensen <jacobrobertjensen@gmail.com>
2020-01-15 13:02:29 -0600

    homework day 3

---
+++ DCDay3.py
@@ -0,0 +1,151 @@
+#lists
+
+# daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+# # print(daysOfWeek[0])
+
+# # daysOfWeek[0] = "Christmas Eve"
+
+# # print(daysOfWeek[0])
+
+# # print(len(daysOfWeek))
+
+# daysOfWeek.append("Funday")
+
+# print(daysOfWeek[7])
+
+# todos = ["pet the cat", "go to work", "shop for groceries", 
+# "go home", "feed the cat"]
+# todos.append("binge watch a show")
+# todos.append("go to sleep")
+
+# index = 0
+
+# while index < len(todos):
+#     print(f"{index + 1}: {todos[index]}")
+#     index += 1
+
+# a = [1, 2, 3, 4]
+
+# b = [5, 6, 7, 8]
+
+# print(a)
+# print(b)
+
+# print(a + b)
+
+
+# todos = ["pet the cat", "go to work", "shop for groceries", 
+# "go home", "feed the cat"]
+
+# todos.extend(["eat dinner", "shower"])
+
+# print(todos)
+
+
+#How do I delete items from a List?
+
+# a = ["1", "2", "3"]
+
+# del a[1]
+
+# print(a)
+
+# b = ["1", "2", "3"]
+
+# del b[1:3]
+
+# print(b)
+
+
+# items = []
+
+# to_add = input('What do you need to add to your to-do list today? ')
+
+# while len(to_add) > 0:
+#         items.append(to_add)
+#         print(items)
+#         to_add = input('What do you need to add to your to-do list today? ')
+# print(items)
+
+
+# numbers = [1, 2, 3, 4, 5]
+
+# print(numbers[2:])
+
+# numbers = [1, 2, 3, 4, 5]
+
+# # numbers.insert(3, 6)
+
+# # print(numbers)
+
+# # numbers.pop(2)
+
+# # print(numbers)
+
+# # while (len(numbers) > 0):
+# #     print(numbers.pop())
+# #     print(numbers)
+# # print('finished')
+
+# result = numbers.index(3)
+# print(result)
+
+
+# students = ['Jaye', 'Austin', 'Daniel', 'Alex']
+
+# result = students.index('Daniel')
+
+# print(result)
+
+
+# myList = [1, 2, 3, 4, 5]
+
+# newList = myList
+
+# newList[0] = "changed"
+
+# print(myList)
+# print(newList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hwday3.py
@@ -0,0 +1,29 @@
+#Homework day 3 Wednesday January 15th 2020
+
+
+#1 Sum the Numbers
+
+# numbers = [1, 2, 3, 4, 5 ,6]
+
+# print("Sum of numbers is: " + str(sum(numbers)))
+
+
+#2 Largest Number
+
+# numbers = [1, 2, 4, 43, 5 ,16]
+
+# numbers.sort()
+
+# print(numbers[-1])
+
+
+#3 Smallest Number
+
+# numbers = [13, 2, 4, 43, 5 ,16]
+
+# numbers.sort()
+
+# print(numbers[0])
+
+
+#4 Even Numbers
