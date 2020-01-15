6438cf1851b8d809853e6de9110c456cd7b28c3c
Jaye Jensen <jacobrobertjensen@gmail.com>
2020-01-15 17:25:36 -0600

    homework day3

--- hwday3.py
+++ hwday3.py
@@ -113,22 +113,93 @@
 
 #3 Matrix Addition II
 
-dim1 = [[12, 44], [33, 65]]
-dim2 = [[43, 67], [89, 33]]
-results = []
+# dim1 = [[12, 44], [33, 65]]
+# dim2 = [[43, 67], [89, 33]]
+# results = []
 
-for d1_index in range(len(dim1)) :
-    for d2_index in range(len(dim2)) :
-        if d1_index == d2_index:
-            working_list = []
-            sum1 = dim1[d1_index][0] + dim2[d2_index][0]
-            sum2 = dim1[d1_index][1] + dim2[d2_index][1]
-            working_list.append(sum1)
-            working_list.append(sum2)
-            results.append(working_list)
-print(results)
+# for d1_index in range(len(dim1)) :
+#     for d2_index in range(len(dim2)) :
+#         if d1_index == d2_index:
+#             working_list = []
+#             sum1 = dim1[d1_index][0] + dim2[d2_index][0]
+#             sum2 = dim1[d1_index][1] + dim2[d2_index][1]
+#             working_list.append(sum1)
+#             working_list.append(sum2)
+#             results.append(working_list)
+# print(results)
 
 
+#4 De-dup
+
+# my_list = [1, 2, 3, 4, 5, 4, 3]
+# new_list = []
+# for i in my_list :
+#     if i not in new_list:
+#         new_list.append(i)
+# print(new_list)
+
+
+#5 Leetspeak
+
+# paragraph = """What were they eating? It didn't taste like anything she had ever eaten before and
+#  although she was famished, she didn't dare ask. She knew the answer would be one she didn't want to hear."""
+# paragraph_upper = paragraph.upper()
+# para_list = list(paragraph_upper)
+# leet_para = []
+# leet = {"A" : "4", "E" : "3", "G" : "6", "I" : "1", "O" : "0", "S" : "5", "T" : "7"}
+
+# for letter in paragraph_upper :
+#     if letter in leet:
+#         letter = leet[letter]
+#         leet_para.append(letter)
+#     else:
+#         leet_para.append(letter)
+# result = "".join(leet_para).lower()
+# print(result)
+
+
+#6 Long Vowels
+
+# word = input("Give me a word! ")
+# word_list = list(word)
+# long_vowels = {"aa" : "aaaaa", "ee" : "eeeee", "ii" : "iiiii", "oo" : "ooooo", "uu" : "uuuuu"}
+# long_word = []
+# for letter in word_list :
+#     in letter in long_vowels:
+#     letter = long_vowels[letter]
+#     long_word.append(letter)
+
+
+#7 Caesar Cipher
+
+# def caesar_encrypt(realText, step):
+#     outText = []
+#     cryptText = []
+
+#     uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+#     lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#     for eachLetter in realText:
+#         if eachLetter in uppercase:
+#             index = uppercase.index(eachLetter)
+#             crypting = (index + step) % 26
+#             cryptText.append(crypting)
+#             newLetter = uppercase[crypting]
+#             outText.append(newLetter)
+#         elif eachLetter in lowercase:
+#             index = lowercase.index(eachLetter)
+#             crypting = (index + step) % 26
+#             cryptText.append(crypting)
+#             newLetter = lowercase[crypting]
+#             outText.append(newLetter)
+#         else:
+#             outText.append(" ")
+
+#     return "".join(outText)
+
+# code = caesar_encrypt('lbh zhfg hayrnea jung lbh unir yrnearq', 13)
+
+# print(code)
 
 
 
