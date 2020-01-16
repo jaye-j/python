76462a6fe9bacad7365cb6ba2e26f00d4d4bd9bb
Jaye Jensen <jacobrobertjensen@gmail.com>
2020-01-16 16:13:50 -0600

    homework day 4

---
+++ hwday4.py
@@ -0,0 +1,85 @@
+
+#Homework day 4 Thursday January 16th 2020
+
+
+# Small
+
+#1 Madlib function
+
+# def madlib(name, subject):
+#     result = (f"{name}\'s favorite subject is {subject}.")
+#     return result
+
+# print(madlib("Jaye", "Science"))
+
+
+#2 Celsius to Farenheit conversion
+
+# def convertTemp(C):
+#     F = (C * 9/5) + 32
+#     return F
+
+# print(convertTemp(24))
+
+
+#3 Farenheit to Celsius conversion
+
+# def convertTemp(F):
+#     C = (F - 32) * 5/9
+#     return C
+
+# print(convertTemp(70))
+
+
+# #4 is_even
+
+# def is_even(n):
+#     if n % 2 == 0:
+#         return True
+#     else:
+#         return False
+# print(is_even(13))
+
+# #5 is_odd
+
+# def is_odd(is_even):
+#     if is_even == True:
+#         return False
+#     else:
+#         return True
+# print(is_odd(is_even))
+
+#6 only_evens
+
+#7 only_odds
+
+
+# Medium
+
+
+#1 Find the smallest number
+
+# def smallest(list_nums):
+#     return min(list_nums)
+# print(smallest([2, 45, 6, 7, 89]))
+
+
+#2 find the largest number
+
+# def largest(list_nums):
+#     return max(list_nums)
+# print(largest([2, 45, 6, 7, 89]))
+
+
+#3 Find the shortest String
+
+# def shortest(string):
+#     return (min(string, key=len))
+# print(shortest(['cat', 'jack', 'it']))
+
+
+#4 Find the longest string
+
+# def longest(string):
+#     return (max(string, key=len))
+# print(longest(['cat', 'jack', 'it']))
