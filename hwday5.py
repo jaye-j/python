e94da3e3ce556071d6849b7ab96f9b1e074b6792
Jaye Jensen <jacobrobertjensen@gmail.com>
2020-01-19 11:43:28 -0600

    hwday5

--- hwday5.py
+++ hwday5.py
@@ -6,12 +6,16 @@
 
 #1 Letter Summary 
 
-user_input = input("Please enter a word: ")
+# user_input = input("Please enter a word: ")
 
-empty_dict = {}
+# empty_dict = {}
 
-for letters in user_input:
-    if letters not in empty_dict:
-        empty_dict[letters] = 0
-    empty_dict[letters] += 1
-print(empty_dict)
+# for letters in user_input:
+#     if letters not in empty_dict:
+#         empty_dict[letters] = 0
+#     empty_dict[letters] += 1
+# print(empty_dict)
+
+
+#2 Word Summary
+
