b9bbd3b036f8f73ff68aea777ee6b0b26e74304d
Jaye Jensen <jacobrobertjensen@gmail.com>
2020-01-19 11:42:18 -0600

    hwday5

--- hwday5.py
+++ hwday5.py
@@ -9,7 +9,9 @@
 user_input = input("Please enter a word: ")
 
 empty_dict = {}
-letter_dump = ""
 
-for letter in user_input:
-    
+for letters in user_input:
+    if letters not in empty_dict:
+        empty_dict[letters] = 0
+    empty_dict[letters] += 1
+print(empty_dict)
