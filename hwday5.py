f5915398e61e60cc74c3c9707b2e936f40056ec4
Jaye Jensen <jacobrobertjensen@gmail.com>
2020-01-20 12:22:23 -0600

    hwday 5

--- hwday5.py
+++ hwday5.py
@@ -19,3 +19,24 @@
 
 #2 Word Summary
 
+# histogram = {}
+# user_input = input("Please enter a sentence: ")
+# words = user_input.split()
+# for repeats in words:
+#     if repeats not in histogram:
+#         histogram[repeats] = 0
+#     histogram[repeats] += 1
+# print(histogram)
+
+
+#3 Sorting a histogram
+
+sent_histogram = {}
+entered_sentence = input("Please enter a sentence... ")
+sent_to_words = entered_sentence.split()
+for repeats in sent_to_words:
+    if repeats not in sent_histogram:
+        sent_histogram[repeats] = 0
+    sent_histogram[repeats] += 1
+for key, value in sorted(sent_histogram.items(), key=lambda kv: kv[1], reverse=True):
+    print("%s: %s" % (key, value))
